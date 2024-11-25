181134543f66aa33b0a9d4688babb48dbc7cbbc8
Shanza Allan <ken22958@kcbgroup.com>
2024-11-25 15:51:16 +0300

    advance sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -3,3 +3,5 @@ FROM Bookings b
 JOIN Users u ON b.user_id = u.id
 JOIN Properties p ON b.property_id = p.id
 LEFT JOIN Payments pay ON b.id = pay.booking_id;
+
+SELECT * FROM Bookings WHERE user_id = 123;
